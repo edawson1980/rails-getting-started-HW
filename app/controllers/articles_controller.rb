ee358676030cd4774229cdbb395d43ab80247dcb
Erin <erinedawson@gmail.com>
2017-04-18 19:02:42 -0400

    finish 5.6

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,16 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(params[:article])
+
+    @article.save
+    redirect to @article
+  end
+
+
+private
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 
 end
