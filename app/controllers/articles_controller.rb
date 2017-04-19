eca464c326b174b329a5b3c940267f0964da15eb
Erin <erinedawson@gmail.com>
2017-04-18 21:11:41 -0400

    finish 5.12

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,11 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+
   def create
     @article = Article.new(article_params)
 
@@ -24,9 +29,19 @@ class ArticlesController < ApplicationController
     end
   end
 
-    private
+  def update
+  @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  private
     def article_params
       params.require(:article).permit(:title, :text)
     end
 
-end
+  end
