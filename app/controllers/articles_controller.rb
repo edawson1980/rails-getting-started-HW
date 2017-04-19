1ea84519d2ff3eaa60daa5559c219e4614d757d1
Erin <erinedawson@gmail.com>
2017-04-18 20:35:22 -0400

    finish 5.8

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,10 @@
 class ArticlesController < ApplicationController
 
+  def index
+      @articles = Article.all
+    end
+
+
   def show
     @article = Article.find(params[:id])
   end
@@ -8,13 +13,13 @@ class ArticlesController < ApplicationController
   def new
   end
 
+
   def create
     @article = Article.new(article_params)
 
     @article.save
     redirect_to @article
   end
-
     private
       def article_params
         params.require(:article).permit(:title, :text)
